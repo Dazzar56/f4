194f3112b8218942e2838837d32bc6636b1b69f7
Ivan Sorokin <unxed@mail.ru>
2026-03-17 10:40:14 +0100

    Combo box

--- demo_dialog.go
+++ demo_dialog.go
@@ -7,9 +7,9 @@ import (
 func ShowDemoDialog() {
 	// Центрируем диалог
 	scrWidth := vtui.FrameManager.GetScreenSize()
-	dlgWidth, dlgHeight := 50, 12
+	dlgWidth, dlgHeight := 60, 15
 	x1 := (scrWidth - dlgWidth) / 2
-	y1 := 5
+	y1 := 4
 
 	dlg := vtui.NewDialog(x1, y1, x1+dlgWidth-1, y1+dlgHeight-1, " vtui Components Demo ")
 
@@ -26,11 +26,19 @@ func ShowDemoDialog() {
 	dlg.AddItem(rb3)
 
 	// Чекбоксы
-	dlg.AddItem(vtui.NewText(x1+26, y1+2, "Settings:", vtui.Palette[vtui.ColDialogText]))
-	dlg.AddItem(vtui.NewCheckbox(x1+28, y1+4, "Enable AI", false))
-	dlg.AddItem(vtui.NewCheckbox(x1+28, y1+5, "Auto-update", true)) // 3rd state demo
+	dlg.AddItem(vtui.NewText(x1+30, y1+2, "Settings:", vtui.Palette[vtui.ColDialogText]))
+	dlg.AddItem(vtui.NewCheckbox(x1+32, y1+4, "Enable AI", false))
+	dlg.AddItem(vtui.NewCheckbox(x1+32, y1+5, "Auto-update", true))
+	dlg.AddItem(vtui.NewCheckbox(x1+32, y1+6, "Force Legacy", false))
 
-	btnOk := vtui.NewButton(x1+dlgWidth/2-5, y1+9, "Close")
+	// ComboBox
+	dlg.AddItem(vtui.NewText(x1+2, y1+8, "Encoding:", vtui.Palette[vtui.ColDialogText]))
+	items := []string{"UTF-8", "CP866 (OEM)", "Windows-1251", "KOI8-R"}
+	combo := vtui.NewComboBox(x1+12, y1+8, 20, items)
+	combo.Edit.SetText("UTF-8")
+	dlg.AddItem(combo)
+
+	btnOk := vtui.NewButton(x1+dlgWidth/2-5, y1+12, "Close")
 	btnOk.OnClick = func() {
 		dlg.SetExitCode(0)
 	}
