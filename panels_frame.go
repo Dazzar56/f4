be6c07e1bdff058d9af0c87ff046300cd8e9285e
Ivan Sorokin <unxed@mail.ru>
2026-04-04 10:39:32 +0200

    Better file naming

--- panels_frame.go
+++ panels_frame.go
@@ -14,6 +14,18 @@ import (
 	"github.com/unxed/vtui"
 )
 
+// A Panel is an interface for any content that can be placed in the "half" of the manager.
+// This could be a file list, a folder tree, or even a quick view panel (Viewer).
+type Panel interface {
+	Show(scr *vtui.ScreenBuf)
+	ProcessKey(e *vtinput.InputEvent) bool
+	ProcessMouse(e *vtinput.InputEvent) bool
+	SetFocus(f bool)
+	IsFocused() bool
+	SetPosition(x1, y1, x2, y2 int)
+	GetPosition() (int, int, int, int)
+	GetSelectedName() string
+}
 // PanelsFrame is the main frame of the f4 manager, containing left and right panels.
 type PanelsFrame struct {
 	vtui.BaseFrame
