77bd96f16bc0a10da4ffd647c3ab73e4b95c2548
Ivan Sorokin <unxed@mail.ru>
2026-04-14 23:49:11 +0200

    Try to fix Windows build

--- vfs/sudo_askpass_windows.go
+++ vfs/sudo_askpass_windows.go
@@ -8,3 +8,11 @@ func RunSudoAskpass() {
 	// Not used on Windows
 	os.Exit(1)
 }
+
+func (c *SudoClient) runAskpassServer(path string) {
+	// Not used on Windows
+}
+
+func (c *SudoClient) RunOnUI(fn func()) {
+	// Not used on Windows
+}
