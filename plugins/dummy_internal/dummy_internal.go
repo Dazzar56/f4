6f10af1673e30a92a47ddfb6ec8f7e72a530244c
Ivan Sorokin <unxed@mail.ru>
2026-04-18 21:48:26 +0200

    Put back internal dummy plugin deleted by mistake

---
+++ plugins/dummy_internal/dummy_internal.go
@@ -0,0 +1,17 @@
+package dummy_internal
+
+import (
+	"fmt"
+	"github.com/unxed/f4/vfs"
+)
+
+type InternalDummyPlugin struct {}
+
+func (p *InternalDummyPlugin) Init(api vfs.HostAPI) error {
+	ver := api.GetVersion()
+	api.Log(fmt.Sprintf("Internal dummy plugin initialized! Host Version: %s", ver))
+	return nil
+}
+
+func (p *InternalDummyPlugin) Close() error { return nil }
+func (p *InternalDummyPlugin) GetName() string { return "Internal Hello World" }
