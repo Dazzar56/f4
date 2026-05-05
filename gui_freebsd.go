c7fa8ddb00d4de29a19c24953d156dff48f1496e
Ivan Sorokin <unxed@mail.ru>
2026-05-05 15:14:09 +0200

    Fix build

--- gui_freebsd.go
+++ gui_freebsd.go
@@ -7,7 +7,6 @@ import (
 	"os"
 )
 
-func RunGui() {
-	fmt.Fprintf(os.Stderr, "GUI mode is currently not supported on FreeBSD.\n")
-	os.Exit(1)
+func RunGui() error {
+	return fmt.Errorf("GUI mode is currently not supported on this platform")
 }
--- gui_windows.go
+++ gui_windows.go
@@ -7,7 +7,6 @@ import (
 	"os"
 )
 
-func RunGui() {
-	fmt.Fprintf(os.Stderr, "GUI mode is currently not supported on Windows.\n")
-	os.Exit(1)
+func RunGui() error {
+	return fmt.Errorf("GUI mode is currently not supported on this platform")
 }
