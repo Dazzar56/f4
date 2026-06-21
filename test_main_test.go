e5f759fb3a3eb26f8631cbfab333f0ca446fe2ec
Ivan Sorokin <unxed@mail.ru>
2026-06-21 14:09:12 +0200

    Disable logging in tests for now

--- test_main_test.go
+++ test_main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/unxed/f4/vfs"
-	"github.com/unxed/vtui"
+	//"github.com/unxed/vtui"
 	"os"
 	"testing"
 )
@@ -11,7 +11,8 @@ func TestMain(m *testing.M) {
 	vfs.InitSudoClient("/usr/bin/f4", "")
 	result := m.Run()
 	if result != 0 {
-		vtui.DumpLogsToFile("_failed_tests_f4.log")
+		// disabled for now
+		//vtui.DumpLogsToFile("_failed_tests_f4.log")
 	}
 	os.Exit(result)
 }
