f69de3631b152f99edc9d87a5b7399e5924a76cd
Ivan Sorokin <unxed@mail.ru>
2026-07-27 19:58:21 +0200

    Revert "Detach from console window on Windows when starting GUI mode using FreeConsole."
    This reverts commit 9863b87b8b4394ccbac77407cb12080f4e8f2774.
    
    Buggy

--- gui_windows.go
+++ gui_windows.go
@@ -6,16 +6,13 @@ import (
 	"strings"
 
 	"github.com/unxed/vtui"
-	"golang.org/x/sys/windows"
 )
 
 func RunGui(backend string) error {
 	if backend == "qt" || strings.HasPrefix(backend, "ext:") {
-		_ = windows.FreeConsole()
 		return RunExternalUIWithMapping(backend)
 	}
 	return vtui.RunInGUIWindow(100, 30, backend, AppConfig.GuiFont, float64(AppConfig.GuiFontSize), func() {
-		_ = windows.FreeConsole()
 		SetupUI()
 	})
 }
