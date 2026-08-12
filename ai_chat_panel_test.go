ede53f61dd89af1fb916b83fcfed6238aaba5d19
Ivan Sorokin <unxed@mail.ru>
2026-08-12 15:22:27 +0200

    Minor

--- ai_chat_panel_test.go
+++ ai_chat_panel_test.go
@@ -10,11 +10,11 @@ func TestAIChatPanel_Resize(t *testing.T) {
 	cp := NewAIChatPanel(fp)
 	cp.SetPosition(0, 0, 79, 23)
 
-	if cp.input.Y1 != 20 {
-		t.Errorf("Expected input Y1 to be 20, got %d", cp.input.Y1)
+	if cp.input.Y1 != 19 {
+		t.Errorf("Expected input Y1 to be 19, got %d", cp.input.Y1)
 	}
-	if cp.input.Y2 != 23 {
-		t.Errorf("Expected input Y2 to be 23, got %d", cp.input.Y2)
+	if cp.input.Y2 != 22 {
+		t.Errorf("Expected input Y2 to be 22, got %d", cp.input.Y2)
 	}
 	if cp.Kind() != "ai_chat" {
 		t.Errorf("Expected Kind 'ai_chat', got '%s'", cp.Kind())
